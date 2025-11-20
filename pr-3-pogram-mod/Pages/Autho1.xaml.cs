94fafc693d89242ab27e35210eda21f0f9d9fae5
Kostyakostya668 <kostyhomyakov0807@gmail.com>
2025-11-20 17:15:49 +0700

    + pr7 / timer

--- pr-3-pogram-mod/Pages/Autho1.xaml.cs
+++ pr-3-pogram-mod/Pages/Autho1.xaml.cs
@@ -5,10 +5,13 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace pr_3_pogram_mod.Pages
 {
@@ -18,23 +21,25 @@ namespace pr_3_pogram_mod.Pages
     public partial class Autho1 : Page
     {
         int click;
+        DispatcherTimer timer = new DispatcherTimer();
+        private int seconds = 10;
 
-        private static bdMod _context;
+        //private static bdMod _context;
 
-        public static bdMod GetContext()
-        {
-            if (_context == null)
-            {
-                _context = new bdMod();
-            }
-            return _context;
-        }
+        //public static bdMod GetContext()
+        //{
+        //    if (_context == null)
+        //    {
+        //        _context = new bdMod();
+        //    }
+        //    return _context;
+        //}
 
         public Autho1()
         {
             InitializeComponent();
             capthaPanel.Visibility = Visibility.Hidden;
-            click = 0;
+            timer.Tick += new EventHandler(timer_Tick);
         }
 
         private void btnEnterGuest_Click(object sender, RoutedEventArgs e)
@@ -45,6 +50,7 @@ namespace pr_3_pogram_mod.Pages
             passwordBox.Clear();
             txtLogin.Clear();
             capthaPanel.Visibility = Visibility.Hidden;
+            click = 0;
         }
 
         private void GenerateCapctcha()
@@ -63,7 +69,7 @@ namespace pr_3_pogram_mod.Pages
             string password = passwordBox.Password.Trim();
             string passwordH = Hash.HashPassword(password);
 
-            bdMod bd = GetContext();
+            bdMod bd = new bdMod();
             
             var user = bd.users.Where(x => x.username == login && x.password == passwordH).FirstOrDefault();
             
@@ -74,6 +80,7 @@ namespace pr_3_pogram_mod.Pages
                     var user_role = bd.user_roles.Where(x => user.role_id == x.id).FirstOrDefault();
                     MessageBox.Show("Вы вошли под: " + user_role.role).ToString();
                     LoadPage(user_role.role, user);
+                    click = 0;
                 }
                 else
                 {
@@ -92,6 +99,7 @@ namespace pr_3_pogram_mod.Pages
                     LoadPage(user_role.role, user);
                     capthaPanel.Visibility = Visibility.Hidden;
                     txtBoxCaptha.Clear();
+                    click = 0;
                 }
                 else
                 {
@@ -102,6 +110,38 @@ namespace pr_3_pogram_mod.Pages
 
                 }
             }
+
+            if (click >= 3)
+            {
+                block(false);
+                timer.Interval = new TimeSpan(0,0,1);
+                timer.Start();
+
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+           seconds--;
+           timeBlock.Content = $"Разблокировка через: {seconds.ToString()}";
+            
+            if (seconds <= 0)
+            {
+                timer.IsEnabled = false;
+                //timer.Tick -= new EventHandler(timer_Tick);
+                seconds = 10;
+                click = 0;
+                block(true);
+            }
+        }
+
+        private void block(bool blockB)
+        {
+            timeBlock.Content = "";
+            txtLogin.IsEnabled = blockB;
+            passwordBox.IsEnabled = blockB;
+            capthaPanel.Visibility = Visibility.Hidden;
+            ButtonPanel.IsEnabled = blockB;
         }
 
         private void LoadPage(string _role, users user)
