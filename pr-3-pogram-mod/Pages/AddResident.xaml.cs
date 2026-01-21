c8cece94f87446c9a7336a24077dc19fe00c560d
Kostyakostya668 <kostyhomyakov0807@gmail.com>
2026-01-21 17:07:28 +0700

    pr 9 / + sort by name, surname

---
+++ pr-3-pogram-mod/Pages/AddResident.xaml.cs
@@ -0,0 +1,116 @@
+﻿using pr_3_pogram_mod.bd;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr_3_pogram_mod.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddResident.xaml
+    /// </summary>
+    public partial class AddResident : Page
+    {
+        private List<int> idPol;
+        //private ObservableCollection<users> _users;
+        //private ObservableCollection<residents> _residents;
+
+        //bool polHas = false;
+
+        public AddResident()
+        {
+            InitializeComponent();
+
+            //var residents = bdMod.GetContext(true).residents.ToList();
+            //_residents = new ObservableCollection<residents>(residents);
+
+            StAddResident.IsEnabled = true;
+        }
+
+        private void btAddRes_Click(object sender, RoutedEventArgs e)
+        {
+            //айди созданного пользователя
+            var userList = bdMod.GetContext(true).users.ToList();
+            List<users> ids = new List<users>();
+            List<int> idPols = new List<int>();
+
+            foreach (var item in userList)
+            {
+                idPols.Add(item.id);
+            }
+
+            int oldId = idPols.Max();
+
+            using (var context = new bdMod())
+            {
+                residents newRes = new residents();
+
+                newRes.name = name.Text;
+                newRes.surname = surname.Text;
+                newRes.phone = phone.Text;
+                newRes.residents_count = Convert.ToInt32(res_count.Text);
+                newRes.account_balance = Convert.ToDecimal(account_bal.Text);
+                newRes.apartment_id = 4;
+
+                context.residents.Add(newRes);
+
+                context.SaveChanges();
+
+                MessageBox.Show("Резидент добавлен", "Инфо", MessageBoxButton.OK);
+            }
+
+            //Console.WriteLine(oldId);   
+
+            //foreach (var item in userList)
+            //{
+            //    idPols.Add(item.id);
+            //    Console.WriteLine(idPols);
+            //}
+
+        }
+
+
+
+        private void btAddPol_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (usernameBox.Text == "" || emailBox.Text == "" || passwordBox.Text == "" || comboBoxRole.SelectedIndex == -1)
+            {
+                MessageBox.Show("Вы не ввели полную информацию", "Ошибка", MessageBoxButton.OK);
+
+                return;
+            }
+
+            using (var context = new bdMod())
+            {
+                users newUser = new users();
+
+                newUser.username = usernameBox.Text;
+                newUser.email = emailBox.Text;
+                newUser.password = Services.Hash.HashPassword(passwordBox.Text);
+                newUser.role_id = 1 + comboBoxRole.SelectedIndex;
+                newUser.is_active = true;
+
+                context.users.Add(newUser);
+
+                context.SaveChanges();
+
+                MessageBox.Show("Пользователь добавлен","Инфо", MessageBoxButton.OK);
+            }
+
+            StAddResident.IsEnabled = true;
+        }
+    }
+}
--- pr-3-pogram-mod/Pages/Admin.xaml.cs
+++ pr-3-pogram-mod/Pages/Admin.xaml.cs
@@ -1,98 +1,247 @@
 ﻿using pr_3_pogram_mod.bd;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace pr_3_pogram_mod.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Admin.xaml
-    /// </summary>
     public partial class Admin : Page
     {
-
         private ObservableCollection<residents> _residents;
-
 
         public Admin(users user, string role, employees employee)
         {
             InitializeComponent();
             textName.Text = $"Пользователь: {role}\n{employee.surname} {employee.name}";
             LoadResidents();
-
+            UpdateListViewTemplate(); 
         }
 
         private void LoadResidents()
         {
             var residents = bdMod.GetContext(true).residents.ToList();
             _residents = new ObservableCollection<residents>(residents);
-            lvRes.ItemsSource = _residents;
+            SortResidents();
+        }
+
+        private void UpdateListViewTemplate()
+        {
+            if (chosenSort == null) return;
+
+            DataTemplate template = null;
+
+            switch (chosenSort.SelectedIndex)
+            {
+                case 1: // имя
+                    template = CreateNameOnlyTemplate();
+                    break;
+                case 2: // фамилия
+                    template = CreateSurnameOnlyTemplate();
+                    break;
+                default: // Просто (оба поля)
+                    template = CreateFullTemplate();
+                    break;
+            }
+
+            if (template != null)
+            {
+                lvRes.ItemTemplate = template;
+            }
+        }
+
+        private DataTemplate CreateFullTemplate()
+        {
+            var template = new DataTemplate();
+
+            var border = new FrameworkElementFactory(typeof(Border));
+            border.SetValue(Border.BorderBrushProperty, System.Windows.Media.Brushes.Black);
+            border.SetValue(Border.BorderThicknessProperty, new Thickness(1));
+            border.SetValue(Border.MarginProperty, new Thickness(5));
+
+            var grid = new FrameworkElementFactory(typeof(Grid));
+            grid.SetValue(Grid.WidthProperty, 120.0);
+
+            var rowDef1 = new FrameworkElementFactory(typeof(RowDefinition));
+            rowDef1.SetValue(RowDefinition.HeightProperty, new GridLength(50));
+            var rowDef2 = new FrameworkElementFactory(typeof(RowDefinition));
+            rowDef2.SetValue(RowDefinition.HeightProperty, new GridLength(60));
+
+            grid.AppendChild(rowDef1);
+            grid.AppendChild(rowDef2);
+
+            var image = new FrameworkElementFactory(typeof(Image));
+            image.SetValue(Image.SourceProperty, new System.Windows.Media.Imaging.BitmapImage(
+                new Uri("/Resources/iconUser.png", UriKind.Relative)));
+            image.SetValue(Grid.RowProperty, 0);
+            image.SetValue(Image.MarginProperty, new Thickness(5));
+
+            var stackPanel = new FrameworkElementFactory(typeof(StackPanel));
+            stackPanel.SetValue(Grid.RowProperty, 1);
+            stackPanel.SetValue(StackPanel.MarginProperty, new Thickness(5));
+
+            var nameText = new FrameworkElementFactory(typeof(TextBlock));
+            var nameBinding = new System.Windows.Data.Binding("name");
+            nameText.SetBinding(TextBlock.TextProperty, nameBinding);
+            nameText.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+            nameText.SetValue(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+
+            var surnameText = new FrameworkElementFactory(typeof(TextBlock));
+            var surnameBinding = new System.Windows.Data.Binding("surname");
+            surnameText.SetBinding(TextBlock.TextProperty, surnameBinding);
+            surnameText.SetValue(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+
+            stackPanel.AppendChild(nameText);
+            stackPanel.AppendChild(surnameText);
+
+            grid.AppendChild(image);
+            grid.AppendChild(stackPanel);
+            border.AppendChild(grid);
+
+            template.VisualTree = border;
+            return template;
+        }
+
+        private DataTemplate CreateNameOnlyTemplate()
+        {
+            var template = new DataTemplate();
+
+            var border = new FrameworkElementFactory(typeof(Border));
+            border.SetValue(Border.BorderBrushProperty, System.Windows.Media.Brushes.Black);
+            border.SetValue(Border.BorderThicknessProperty, new Thickness(1));
+            border.SetValue(Border.MarginProperty, new Thickness(5));
+
+            var stackPanel = new FrameworkElementFactory(typeof(StackPanel));
+            stackPanel.SetValue(StackPanel.WidthProperty, 120.0);
+
+            var image = new FrameworkElementFactory(typeof(Image));
+            image.SetValue(Image.SourceProperty, new System.Windows.Media.Imaging.BitmapImage(
+                new Uri("/Resources/iconUser.png", UriKind.Relative)));
+            image.SetValue(Image.HeightProperty, 40.0);
+            image.SetValue(Image.MarginProperty, new Thickness(5));
+
+            var nameText = new FrameworkElementFactory(typeof(TextBlock));
+            var nameBinding = new System.Windows.Data.Binding("name");
+            nameText.SetBinding(TextBlock.TextProperty, nameBinding);
+            nameText.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+            nameText.SetValue(TextBlock.FontSizeProperty, 14.0);
+            nameText.SetValue(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+            nameText.SetValue(TextBlock.MarginProperty, new Thickness(0, 5, 0, 10));
+
+            stackPanel.AppendChild(image);
+            stackPanel.AppendChild(nameText);
+            border.AppendChild(stackPanel);
+
+            template.VisualTree = border;
+            return template;
+        }
+
+        private DataTemplate CreateSurnameOnlyTemplate()
+        {
+            var template = new DataTemplate();
+
+            var border = new FrameworkElementFactory(typeof(Border));
+            border.SetValue(Border.BorderBrushProperty, System.Windows.Media.Brushes.Black);
+            border.SetValue(Border.BorderThicknessProperty, new Thickness(1));
+            border.SetValue(Border.MarginProperty, new Thickness(5));
+
+            var stackPanel = new FrameworkElementFactory(typeof(StackPanel));
+            stackPanel.SetValue(StackPanel.WidthProperty, 120.0);
+
+            var image = new FrameworkElementFactory(typeof(Image));
+            image.SetValue(Image.SourceProperty, new System.Windows.Media.Imaging.BitmapImage(
+                new Uri("/Resources/iconUser.png", UriKind.Relative)));
+            image.SetValue(Image.HeightProperty, 40.0);
+            image.SetValue(Image.MarginProperty, new Thickness(5));
+
+            var surnameText = new FrameworkElementFactory(typeof(TextBlock));
+            var surnameBinding = new System.Windows.Data.Binding("surname");
+            surnameText.SetBinding(TextBlock.TextProperty, surnameBinding);
+            surnameText.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+            surnameText.SetValue(TextBlock.FontSizeProperty, 14.0);
+            surnameText.SetValue(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+            surnameText.SetValue(TextBlock.MarginProperty, new Thickness(0, 5, 0, 10));
+
+            stackPanel.AppendChild(image);
+            stackPanel.AppendChild(surnameText);
+            border.AppendChild(stackPanel);
+
+            template.VisualTree = border;
+            return template;
+        }
+
+        private void SortResidents()
+        {
+            if (_residents == null) return;
+
+            if (chosenSort.SelectedIndex == 1)
+            {
+                lvRes.ItemsSource = _residents.OrderBy(r => r.name).ToList();
+            }
+            else if (chosenSort.SelectedIndex == 2)
+            {
+                lvRes.ItemsSource = _residents.OrderBy(r => r.surname).ToList();
+            }
+            else
+            {
+                lvRes.ItemsSource = _residents.ToList();
+            }
+        }
+
+        private void chosenSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateListViewTemplate(); 
+            SortResidents(); 
         }
 
         private void tbNameFind_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string name = tbNameFind.Text;
+            string searchText = tbNameFind.Text.ToLower().Trim();
 
-            var resList = bdMod.GetContext(true).residents.ToList();
-            List<residents> names = new List<residents>();
-
-
-            foreach (var item in resList)
+            if (string.IsNullOrEmpty(searchText))
             {
-                if (name.ToLower().Trim() == item.name.ToLower())
-                {
-                    names.Add(item);
-                }
+                SortResidents();
+                return;
             }
 
-            if (names.Any())
-                lvRes.ItemsSource = names;
-            else
-                lvRes.ItemsSource = resList;
+            var filtered = _residents
+                .Where(r => r.name.ToLower().Contains(searchText) ||
+                           r.surname.ToLower().Contains(searchText))
+                .ToList();
+
+            lvRes.ItemsSource = filtered;
         }
 
         private void lvRes_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var elem = lvRes.SelectedItem as residents;
-
             if (elem != null)
             {
                 NavigationService.Navigate(new Pages.customeRes(elem));
-            }
-            else
-            {
-                elem = null;
             }
         }
 
         private void updateInfo()
         {
             var freshData = bdMod.GetContext(true).residents.ToList();
-
             _residents.Clear();
             foreach (var item in freshData)
             {
                 _residents.Add(item);
             }
+            SortResidents();
         }
 
         private void brRes_Click(object sender, RoutedEventArgs e)
         {
             updateInfo();
+        }
+
+        private void btAddRes_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.AddResident());
         }
     }
 }
